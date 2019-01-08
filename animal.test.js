eb2b0262ee8af59709127e852b95dbd99e57a8bb
Aaron Dennis <AMDennis1987@gmail.com>
2019-01-08 11:07:23 -0800

    moved sayHi into expect

--- animal.test.js
+++ animal.test.js
@@ -1,25 +1,24 @@
 const { AnimalClass, AnimalFactory, AnimalConstructor } = require('./animal');
 
-describe('implementation', () => {
+describe('animal creators', () => {
   describe('AnimalClass', () => {
     it('returns a string of animals species and name', () => {
-      const results = new AnimalClass('Beatrice', 'Snake').sayHi();
-      expect(results).toEqual('Hello, I am a Snake and my name is Beatrice');
+      const results = new AnimalClass('Beatrice', 'Snake');
+      expect(results.sayHi()).toEqual('Hello, I am a Snake and my name is Beatrice');
     });
   });
   describe('AnimalFactory', () => {
     it('returns a string of animals species and name', () => {
-      const results = new AnimalFactory('Billy', 'Buffalo').sayHi();
-      expect(results).toEqual('Hello, I am a Buffalo and my name is Billy');
+      const results = new AnimalFactory('Billy', 'Buffalo');
+      expect(results.sayHi()).toEqual('Hello, I am a Buffalo and my name is Billy');
     });
   });
   describe('AnimalConstructor', () => {
     it('returns a string of animals species and name', () => {
-      const results = new AnimalConstructor('Buff', 'Lamb').sayHi();
-      expect(results).toEqual('Hello, I am a Lamb and my name is Buff');
+      const results = new AnimalConstructor('Buff', 'Lamb');
+      expect(results.sayHi()).toEqual('Hello, I am a Lamb and my name is Buff');
     });
   });
-
 });
 
 
