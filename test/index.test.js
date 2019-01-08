9dd2aa84eebfbda975020886bd85496f6afe536c
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-08 13:48:51 -0800

    add shortid and move in-class demos to folder

---
+++ test/index.test.js
@@ -0,0 +1,30 @@
+const MemoryDatabase = require('../lib/index');
+
+describe('memory database', () => {
+
+  let db = null;
+
+  beforeEach(() => {
+    db = new MemoryDatabase();
+    // runs before each test it
+    // * clear database
+    // * setup required for each test
+  });
+
+
+
+  it('creates an object in the database', () => {
+    const cat = { name: 'fluffy' };
+    const createdCat = db.create(cat);
+    expect(createdCat.name).toEqual('fluffy');
+  });
+
+  // it('can find object by id', () => {
+  //   const cat = { name: 'fluffy' };
+  //   const createdCat = db.create(cat);
+
+  //   const foundCat = db.findById(createdCat._id);
+  //   expect(foundCat).toEqual(createdCat);
+  // });
+
+});
