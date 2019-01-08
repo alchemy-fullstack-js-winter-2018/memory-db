8a5133195540f78e9c14946768156e511fecf4db
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-08 14:21:45 -0800

    find method working now

--- test/index.test.js
+++ test/index.test.js
@@ -14,11 +14,38 @@ describe('memory database', () => {
       const cat = { name: 'fluffy' };
       const createdCat = db.create(cat);
       expect(createdCat.name).toEqual('fluffy');
+    });
+  });
   
+  describe('findById', () => {
+    it('can find an object by id', () => {
+      const cat = { name: 'fluffy' };
+      const createdCat = db.create(cat);
+      // -> {name: 'fluffy', _id: 1234}
+      // createdCat._id === 1234
+      const storedCat = db.findById(createdCat._id);
+      expect(storedCat).toEqual(createdCat);
+    });
+  
+    it('can throw an error if there is no object with matching id', () => {
+      try {
+        const dog = { name: 'fluffy', id: '99999' };
+        expect(db.findById(dog.id)).toThrowError();
+      }
+      catch(error) {
+        expect(error).toEqual('error no matching object with given id');
+      }
     });
   });
 
-  it('test 2', () => {
-
+  describe('find', () => {
+    it('can return all objects within the object', () => {
+      const cat1 = { name: 'fluffy' };
+      const createdCat1 = db.create(cat1);
+      const cat2 = { name: 'spot' };
+      const createdCat2 = db.create(cat2);
+      const data = [createdCat1, createdCat2];
+      expect(db.find()).toEqual(data);
+    });
   });
 });
