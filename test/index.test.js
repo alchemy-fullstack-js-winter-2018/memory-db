6a774140173cda8332d7de95f4ca525c97b3e38d
Sophie Graham <sophiergraham1@gmail.com>
2019-01-08 14:53:31 -0800

    fixed linter errors

--- test/index.test.js
+++ test/index.test.js
@@ -1,36 +1,49 @@
 const MemoryDatabase = ('../lib/index');
 
-describe('memory database' () => {
+describe('memory database', () => {
   let db = null;
   beforeEach(() => {
-    db = new MemoryDatabase()
+    db = new MemoryDatabase();
   });
-    //run before each test (it)
-    //*clear database
-    //*setup required for each test
-    it('creates an object in the database', () => {
-      const cat = { name: 'fluffy' };
-      const createdCat = db.create(cat);
-      expect(createdCat.name).toEqual('fluffy')
-    });
+   
+  it('creates an object in the database', () => {
+    const cat = { name: 'fluffy' };
+    const createdCat = db.create(cat);
+    expect(createdCat.name).toEqual('fluffy');
+  });
 
-    it('can find an object by id', () => {
-      const cat = {name: 'fluffy' };
-      const createdCat = db.create(cat)
-    });
+  it('can find an object by id', () => {
+    const cat = { name: 'fluffy' };
+    const createdCat = db.create(cat);
+
+    const foundCat = db.findById(createdCat._id);
+    expect(foundCat).toEqual(createdCat);
   });
-  // beforeAll(() => {
-  //   //once before the first test runs
-  //   //*starting our database
-  //   //*getting credentials
-  // });
-  // afterEach(() => {
-  //   //run after each test (it)
-  //   //*clean up between tests
-  //   //*closing db connections
-  // });
-  // afterAll(() => {
-  //   //run after the last test runs
-  //   //*deleting files that were created during testing
-  //   //*shutdown db
-  // });
+
+  it('throws an error when trying to find sn object that does not exist', () => {
+    expect(() => {
+      db.findById('notARealId');
+    }).toThrowError('No object with _id notARealId');
+  });
+});
+
+// beforeEach(() => {
+//run before each test (it)
+//*clear database
+//*setup required for each test
+// });
+// beforeAll(() => {
+//   //once before the first test runs
+//   //*starting our database
+//   //*getting credentials
+// });
+// afterEach(() => {
+//   //run after each test (it)
+//   //*clean up between tests
+//   //*closing db connections
+// });
+// afterAll(() => {
+//   //run after the last test runs
+//   //*deleting files that were created during testing
+//   //*shutdown db
+// });
