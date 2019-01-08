325f4fc1191e23f65bdc26dae701bd359efd4533
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-08 13:54:18 -0800

    found by Id

---
+++ test/index.test.js
@@ -0,0 +1,23 @@
+const MemoryDatabase = require('../lib/index');
+
+describe('memory database', () => {
+  let db = null;
+  beforeEach(() => {
+    db = new MemoryDatabase();
+  });
+
+  it('creates an object in the db', () => {
+    const cat = { name: 'fluffy' };
+    const createdCat = db.create(cat);
+    expect(createdCat.name).toEqual('fluffy');
+  });
+
+  it('can find an object by id', () => {
+    const cat = { _id: 123, name: 'fluffy' };
+    const createdCat = db.create(cat);
+
+    
+    const foundCat = db.findById(createdCat._id);
+    expect(foundCat).toEqual(createdCat);
+  });
+});
