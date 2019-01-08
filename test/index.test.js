cb97b6038cf5f843f8f15bd9b7801e590c46ff63
Carmen Perezchica <crperezchica@gmail.com>
2019-01-08 15:21:57 -0800

    create, id and findid test passing

--- test/index.test.js
+++ test/index.test.js
@@ -3,28 +3,39 @@ const MemoryDatabase = require('../lib/index');
 describe('memory database', () => {
     let db = null;
     beforeEach(() => { 
-        db = new MemoryDatabase;
-    })
+        db = new MemoryDatabase();
 });
 
 //store = {{ name: 'fluffy' }}
 
-it('creates an object in the database', () => {
-    const cat = { name: 'fluffy'};
+    it('creates an object in the database', () => {
+    const cat = { name: 'fluffy' };
     const createdCat = db.create(cat);
     expect(createdCat.name).toEqual('fluffy');
-});
+    });
 
     it('can find an object by Id', () => {
         const cat = { name: 'fluffy' };
-        const createCat = db.create(cat);
-        // -> {name:'fluffy', _id:1234}
-        //createdCat._id = 1234
+        const createdCat = db.create(cat);
 
-        const storedCat = db.findById(createCat._id);
-        expect(storedCat).toEqual(createdCat);
+
+        const foundCat = db.findById(createdCat._id);
+        expect(foundCat).toEqual(createdCat);
+     });
+
+     it('throws an error when trying to find an object that does not exist', () =>{
+        expect(() => {
+            db.findById('notARealId');
+        }).toThrowError();
     });
 
-    
+    // it('will return a list of all objects', () => {
+        
+    //     const cats = [{ 
+    //         name: 'snowball', 
+    //         name: 'pinky'
+    //     }]
 
+
+    // })
 });
