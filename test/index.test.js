77545e31295c9567809a868d5300c611123066b5
Sophie Graham <sophiergraham1@gmail.com>
2019-01-09 09:03:08 -0800

    submission

--- test/index.test.js
+++ test/index.test.js
@@ -27,25 +27,42 @@ describe('memory database', () => {
   });
 
   it('returns a list of objects', () => {
-    const cat = { name: 'fluffy' };
-    const createCat1 = db.create(cat);
+    const cat1 = { name: 'fluffy' };
+    const createCat1 = db.create(cat1);
     const cat2 = { name: 'whiskers' };
     const createCat2 = db.create(cat2);
     expect(db.find()).toEqual([createCat1, createCat2]);
   });
 
   it('finds by ID and updates', () => {
-    const cat = { name: 'fluffy' };
-    const createdCat = db.create(cat);
+    const obj1 = { name: 'obj1' };
+    const obj2 = { name: 'obj2' };
+    const createdObj1 = db.create(obj1);
+    const createdObj2 = db.create(obj2);
+    let updatedItem = db.findByIdAndUpdate(createdObj1._id, createdObj2);
+    updatedItem.name = obj2.name;
+    expect(updatedItem).toEqual(createdObj2);
+  });
 
-    const foundCat = db.findById(createdCat._id);
-    expect(foundCat).toEqual(createdCat);
+  it('throws an error if no object exists for the id', () => {
+    expect(() => {
+      db.findByIdAndUpdate('notARealId');
+    }).toThrowError('No object exists for this id');
   });
 
   it('finds by ID and deletes', () => {
-
+    const obj = { name: 'obj1' };
+    const createdObj = db.create(obj);
+    findByIdAndDelete = findByIdAndDelete(obj1, _id, )
+    return { deleted: 1 };
   });
 
+  it('throws an error if no object exists for the id', () => {
+    expect(() => {
+      db.findByIdAndDelete('notARealId');
+    }).toThrowError('No object exists for this id');
+  });
+  
   it('drops', () => {
 
   });
