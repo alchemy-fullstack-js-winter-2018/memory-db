dfea294dbbf66a17e08692f8e6319d41806e68b5
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-10 08:39:53 -0800

    added whitespace to tests. moved files into test and lib

--- test/index.test.js
+++ test/index.test.js
@@ -1,26 +1,30 @@
-const MemoryDatabase = require('./index');
+const MemoryDatabase = require('../lib/index');
 
 describe('memory database', () => {
   let db = null;
   beforeEach(() => {
     db = new MemoryDatabase();
   });
+
   it('creates an object in the databse', () => {
     const cat = { name: 'fluffy' };
     const createdCat = db.create(cat);
     expect(createdCat.name).toEqual('fluffy');
   });
+
   it('can find an object by id', () => {
     const cat = { _id: 123, name: 'fluffy' };
     const createdCat = db.create(cat);
     const foundCat = db.findById(createdCat._id);
     expect(foundCat).toEqual(createdCat);
   });
+
   it('throws an error when trying to find an object that does not exist', () => {
     expect(() => {
       db.findById('notARealId');
     }).toThrowError('No object with _id notARealId');
   });
+
   it('can return every object in the store', () => {
     const obj1 = { name: 'foo' };
     const obj2 = { name: 'bar' };
@@ -30,27 +34,30 @@ describe('memory database', () => {
     expect(foundObjs[0].name).toEqual('foo');
     expect(foundObjs[1].name).toEqual('bar');
   });
+
   it('throws error if wrong id when updating', () => {
     expect(() => {
       db.findById('NotARealId');
     }).toThrowError();
   });
+
   it('throws an error if invalid ID', () => {
     expect(() => {
       db.findById('notARealId');
     }).toThrowError();
   });
+
   it('finds an object by id and then deletes it', () => {
     const cat = { name: 'cat3' };
     const createdCat = db.create(cat);
     const deletedItem = db.findByIdAndDelete(createdCat._id);
     expect(deletedItem).toEqual({ deleted: 1 });
   });
+
   it('deletes everything in the store', () => {
     const cat = { name: 'cat3' };
     db.create(cat);
     db.drop();
     expect(db.store).toEqual({});
-
   });
 });
