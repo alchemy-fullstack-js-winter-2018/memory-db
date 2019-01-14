b224feafd5598b6295e5213984400249c20feccf
JeiWolfei <jvampy22@gmail.com>
2019-01-14 14:31:47 -0800

    starting over

--- test/index.test.js
+++ test/index.test.js
@@ -6,77 +6,58 @@ describe('memory database', () => {
     db = new MemoryDatabase();
   });
 
-  describe('create', () => {
-    it('creates an object in the database', () => {
-      const cat = { name: 'fluffy' };
-      const createdCat = db.create(cat);
-      expect(createdCat.name).toEqual('fluffy');
-    });
+  it('creates an object in the databse', () => {
+    const cat = { name: 'fluffy' };
+    const createdCat = db.create(cat);
+    expect(createdCat.name).toEqual('fluffy');
   });
 
-  describe('findById', () => {
-    it('can find an object by id', () => {
-      const cat = { name: 'puffy' };
-      const createdCat = db.create(cat);
-      const storedCat = db.findById(createdCat._id);
-      expect(storedCat).toEqual(createdCat);
-    });
-
-    it('throws an error when trying to find an object that does not exist', () => {
-      expect(() => {
-        db.findById('notARealId');
-      }).toThrowError('No object with _id ${id}');
-    });
+  it('can find an object by id', () => {
+    const cat = { _id: 123, name: 'fluffy' };
+    const createdCat = db.create(cat);
+    const foundCat = db.findById(createdCat._id);
+    expect(foundCat).toEqual(createdCat);
   });
 
-  describe('find', () => {
-    it('can return an object within the object', () => {
-      const cat1 = { name: 'buffy' };
-      const createdCat1 = db.create(cat1);
-      const cat2 = { name: 'muffy' };
-      const createdCat2 = db.create(cat2);
-      const data = [createdCat1, createdCat2];
-      expect(db.find()).toEqual(data);
-    });
-
-    it('can return an empty array when the store is empty', () => {
-      expect(db.find()).toEqual([]);
-    });
+  it('throws an error when trying to find an object that does not exist', () => {
+    expect(() => {
+      db.findById('notARealId');
+    }).toThrowError('No object with _id notARealId');
   });
 
-  describe('findByIdAndUpdate', () => {
-    it('can find object by id and update it', () => {
-      const cat = { name: 'scruffy' };
-      const createdCat = db.create(cat);
-      const newCat = { name: 'fifi' };
-      expect(db.findByIdAndUpdate(createdCat._id, newCat)).toEqual({ name: 'fifi', _id: createdCat._id });
-    });
-    it('can throw an error if there is no object with matching id', () => {
-      expect(() => {
-        db.findById('fakeId');
-      }).toThrowError('No object with _id ${id}');
-    });
-  });
-    
-  describe('findByIdAndDelete', () => {
-    it('can find object by id and delete it', () => {
-      const cat = { name: 'biff' };
-      const createdCat = db.create(cat);
-      expect(db.findByIdAndDelete(createdCat._id)).toEqual({ deleted: 1});
-    });
-
-    it('can throw an error if there is no object with matching id', () => {
-      expect(() => {
-        db.findById('noIdMatch');
-      }).toThrowError('No object with _id ${id}');
-    });
+  it('can return every object in the store', () => {
+    const obj1 = { name: 'fifi' };
+    const obj2 = { name: 'ralph' };
+    db.create(obj1);
+    db.create(obj2);
+    const foundObjs = db.find();
+    expect(foundObjs[0].name).toEqual('fifi');
+    expect(foundObjs[1].name).toEqual('ralph');
   });
 
-  describe('drop', () => {
-    it('can delete all keys in store', () => {
-      const cat = { name: 'walter' };
-      const createdCat = db.create(cat);
-      expect(db.drop(createdCat)).toEqual({});
-    });
+  it('throws error if wrong id when updating', () => {
+    expect(() => {
+      db.findById('NotARealId');
+    }).toThrowError();
+  });
+
+  it('throws an error if invalid ID', () => {
+    expect(() => {
+      db.findById('notARealId');
+    }).toThrowError();
+  });
+
+  it('finds an object by id and then deletes it', () => {
+    const cat = { name: 'cat3' };
+    const createdCat = db.create(cat);
+    const deletedItem = db.findByIdAndDelete(createdCat._id);
+    expect(deletedItem).toEqual({ deleted: 1 });
+  });
+
+  it('deletes everything in the store', () => {
+    const cat = { name: 'cat3' };
+    db.create(cat);
+    db.drop();
+    expect(db.store).toEqual({});
   });
 });
